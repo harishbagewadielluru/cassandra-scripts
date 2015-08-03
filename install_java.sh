65692a458a759785f30129d0b642ec22796746d3
Harish Bagewadi Elluru <harishbe88@yahoo.com>
2015-08-03 12:53:26 -0700

    ++

---
+++ install_java.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.rpm
+sudo rpm -Uvh jdk-7u51-linux-x64.rpm
+sudo alternatives --install /usr/bin/java java /usr/java/jdk1.7.0_51/jre/bin/java 20000
+sudo alternatives --set java /usr/java/jdk1.7.0_51/jre/bin/java
