2d3d7767a920ba6913c4c7d398895558b36a250d
Harish Bagewadi Elluru <harishbe88@yahoo.com>
2015-08-12 16:18:44 -0700

    ++

--- enable_jmx_auth.sh
+++ enable_jmx_auth.sh
@@ -6,7 +6,7 @@ sudo sed -i "/jmxremote.password.file/d" /etc/dse/cassandra/cassandra-env.sh
 sudo sed -i '285 a JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.password.file=/etc/dse/cassandra/jmxremote.password"' /etc/dse/cassandra/cassandra-env.sh
 sudo cp `sudo find / -name jmxremote.password.template | grep "/usr/java"` /etc/dse/cassandra/jmxremote.password
 sudo chmod a+w /etc/dse/cassandra/jmxremote.password
-sudo echo "cassandra XXXXXXX" >> /etc/dse/cassandra/jmxremote.password
+sudo echo "cassandra $1" >> /etc/dse/cassandra/jmxremote.password
 sudo chown cassandra:cassandra /etc/dse/cassandra/jmxremote.password
 sudo chmod 400 /etc/dse/cassandra/jmxremote.password
 sudo sed -i '/cassandra/d' `sudo find / -name jmxremote.access | grep "/usr/java"`
