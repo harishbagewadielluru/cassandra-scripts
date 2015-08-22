aee169759d62df73453b740eef49faf627ba33bd
Harish Elluru <helluru@clx-lta-ops001.orionhealth.saas>
2015-08-21 18:50:50 -0700

    ++

---
+++ install_opscenter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USAGE="Usage: install_opscenter.sh username password version"
+if [ $# -lt 3 ] ; then
+echo $USAGE
+exit 1;
+fi
+
+username=$1
+password=$2
+reponame=datastax
+url=http://username:password@rpm.datastax.com/enterprise
+version=$3
+echo "Creating Datastax Repo..."
+sudo touch /etc/yum.repos.d/$reponame.repo
+sudo chmod a+w /etc/yum.repos.d/$reponame.repo
+sudo echo -e "[$reponame]\nbaseurl=${url}\nenabled=1\ngpgcheck=0" > /etc/yum.repos.d/$reponame.repo
+sudo chmod 644 /etc/yum.repos.d/$reponame.repo
+echo "Installing Opscenter"
+sudo yum clean all
+sudo yum -y install opscenter-$version
