f870842906ca465884abac7776db15e9a2d75599
Harish Bagewadi Elluru <harishbe88@yahoo.com>
2017-03-13 11:58:11 -0700

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.rpm
-udo wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm
-sudo rpm -Uvh jdk-7u51-linux-x64.rpm
-sudo alternatives --install /usr/bin/java java /usr/java/jdk1.7.0_51/jre/bin/java 20000
-sudo alternatives --set java /usr/java/jdk1.7.0_51/jre/bin/java
+sudo wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm
+sudo rpm -Uvh jdk-8u112-linux-x64.rpm
+sudo alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_112/jre/bin/java 20000
+sudo alternatives --set java /usr/java/jdk1.8.0_112/jre/bin/java
